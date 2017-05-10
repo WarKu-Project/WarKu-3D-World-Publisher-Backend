2970fe2fcdd55547acde05cb76e4fed7b138b2cf
Voraton Lertrattanapaisal <iguru.shou.5410070@gmail.com>
2017-05-10 17:47:28 +0700

    Ready to handshake

--- src/packet.js
+++ src/packet.js
@@ -7,7 +7,40 @@ let PacketWriter = require('dgt-net').packet_writer
 * Initialize Packet
 **/
 let packet = {
+  CLIENT_REQUEST_HANDSHAKE : 10000,
 
+  SERVER_RESPONSE_ACCEPT_HANDSHAKE : 20000
+}
+
+/**
+* Receive handshake request from client
+**/
+packet[packet.CLIENT_REQUEST_HANDSHAKE] = (remote,pr)=>{
+  let username = pr.read_string()
+  remote.handshake(username)
+}
+
+/**
+* Response Acceptance handshking to Client
+* @param pX is x-axis position of user
+* @param pY is y-axis position of user
+* @param cX is x-axis position of castle
+* @param cY is y-axis position of castle
+* @param cR is rotate of castle
+* @param cH is health point of castle
+* @param cC is conqueror of castle
+**/
+packet.responseAcceptHandShake = (pX,pY,cX,cY,cR,cH,cC)=>{
+  let pw = new PacketWriter(packet.SERVER_RESPONSE_ACCEPT_HANDSHAKE)
+  pw.append_float(pX)
+  pw.append_float(pY)
+  pw.append_float(cX)
+  pw.append_float(cY)
+  pw.append_float(cR)
+  pw.append_float(cH)
+  pw.append_string(cC)
+  pw.finish()
+  return pw.buffer
 }
 
 module.exports = packet;
--- src/remote.js
+++ src/remote.js
@@ -31,7 +31,59 @@ class Client extends RemoteProxy {
   onDisconnected() {
     console.log("RemoteProxy Disconnected from " + this.getPeerName())
   }
-
+  /**
+  * Do handshaking and save client username
+  **/
+  handshake(username){
+    this.username = username
+    let self = this
+    this.assignPlayerPosition((pX,pY)=>{
+      self.fetchCastleInfo((cX,cY,cR,cH,cC)=>{
+        self.responseAcceptHandShake(pX,pY,cX,cY,cR,cH,cC)
+      })
+    })
+  }
+  /**
+  * Response Success handshaking to user
+  **/
+  responseAcceptHandShake(pX,pY,cX,cY,cR,cH,cC){
+    this.send(packet.responseAcceptHandShake(pX,pY,cX,cY,cR,cH,cC))
+  }
+  /**
+  * Assign Position for player
+  **/
+  assignPlayerPosition(cb){
+    let direction = Math.floor(Math.random()*4)
+    let pX = 0
+    let pY = 0
+    if (direction == 0){
+      pY = Math.random()*50+50
+      pX = Math.random()*400+50
+    }else if (direction == 1){
+      pX = Math.random()*50+50
+      pY = Math.random()*400+50
+    }else if (direction == 2){
+      pY = Math.random()*400+50
+      pX = Math.random()*50+400
+    }else if (direction == 3){
+      pY = Math.random()*50+400
+      pX = Math.random()*400+50
+    }
+    cb(pX,pY)
+  }
+  /**
+  * fetch infomation of castle
+  **/
+  fetchCastleInfo(cb){
+    mongodb.find('world',{attr:'castle'},(result)=>{
+      let cX = result[0].value.position.x
+      let cY = result[0].value.position.y
+      let cR = result[0].value.rotation
+      let cH = result[0].value.progress
+      let cC = result[0].value.conqueror
+      cb(cX,cY,cR,cH,cC)
+    })
+  }
 }
 
 module.exports = Client
