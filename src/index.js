9ee44fc81e4438b2e794d18250a8282026d5a03b
Voraton Lertrattanapaisal <iguru.shou.5410070@gmail.com>
2017-05-15 22:05:50 +0700

    Change Structure

--- src/index.js
+++ src/index.js
@@ -2,9 +2,30 @@
 * Receive PORT from process
 **/
 process.PORT = process.argv[2]
-
 /**
 * Initialize Server
 **/
-let server = require('./server')
+let server = require('./server/server')
 server.init(process.PORT)
+/**
+* Import MongoDB
+**/
+let mongodb = require('../mongodb')
+/**
+* Terminate Condition
+**/
+process.on ('uncaughtException', err => {
+  mongodb.update('server',{ type:'world-child',port:process.PORT},{status:'Disconnected'},()=>{
+    process.exit(1)
+  })
+})
+process.on ('SIGINT', () => {
+  mongodb.update('server',{ type:'world-child',port:process.PORT},{status:'Disconnected'},()=>{
+    process.exit(0)
+  })
+})
+process.on('exit',code=>{
+  mongodb.update('server',{ type:'world-child',port:process.PORT},{status:'Disconnected'},()=>{
+    process.exit(code)
+  })
+})
